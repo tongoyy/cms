4c7622fe495778105eacbd757d352f9f8b421320
tongoyy <ariefm012@gmail.com>
2025-02-06 18:14:06 +0700

    lagi bikin po

--- database/migrations/2025_02_05_105225_create_purchase_orders_table.php
+++ database/migrations/2025_02_05_105225_create_purchase_orders_table.php
@@ -14,21 +14,23 @@ return new class extends Migration
         Schema::create('purchase_orders', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('Purchase_Requests_ID');
+            $table->unsignedBigInteger('Vendors_ID'); // New foreign key column
             $table->bigInteger('Number');
             $table->text('PO_Code');
             $table->text('PO_Name');
-            $table->text('Vendors');
             $table->text('Purchase_Request');
             $table->date('Order_Date');
             $table->text('Department');
             $table->text('Category');
             $table->text('Project');
             $table->foreign('Purchase_Requests_ID')->references('id')->on('purchase_requests');
+            $table->foreign('vendors_id')->references('id')->on('vendors'); // Foreign key constraint
             $table->text('SubTotal');
             $table->text('GrandTotal');
             $table->timestamps();
         });
     }
+
 
     /**
      * Reverse the migrations.
